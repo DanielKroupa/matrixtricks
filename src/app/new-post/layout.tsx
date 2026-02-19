73ded7ac8c030e24e2a9398e11b069fc410549b4
Daniel Kroupa <d_kroupa@utb.cz>
2026-02-19 17:25:42 +0100

    secure new-post page with user session validation and admin access

--- src/app/new-post/layout.tsx
+++ src/app/new-post/layout.tsx
@@ -1,14 +1,31 @@
+import { getServerSession } from "@/lib/get-session";
 import { Banner } from "../components/Banner";
 import { Footer } from "../components/pageLayout/Footer";
 import { Navbar } from "../components/pageLayout/Navbar";
 import { Title } from "../components/Title";
 import { Metadata } from "next";
 
+import { unauthorized, forbidden } from "next/navigation";
+
 export const metadata: Metadata = {
   title: "New Post | Matrix Tricks",
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession();
+  const user = session?.user;
+
+  if (!user) {
+    unauthorized();
+  }
+  if (user?.role !== "admin") {
+    forbidden();
+  }
+
   return (
     <div className="mx-auto min-h-screen text-black dark:bg-neutral-800 dark:text-white">
       <Navbar />
